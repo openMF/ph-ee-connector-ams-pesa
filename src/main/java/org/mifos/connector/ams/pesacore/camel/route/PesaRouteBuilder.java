6aaa9634d595d8baf6e15c00a73af57a38aaa39d
Ishan Ranasingh <44061541+truthfool@users.noreply.github.com>
2023-02-17 19:11:54 +0530

    Paybill validation (#20)
    * paybilll validation
    
    * changed logger levels
    
    * changed application yaml

--- src/main/java/org/mifos/connector/ams/pesacore/camel/route/PesaRouteBuilder.java
+++ src/main/java/org/mifos/connector/ams/pesacore/camel/route/PesaRouteBuilder.java
@@ -7,6 +7,7 @@ import org.apache.camel.model.dataformat.JsonLibrary;
 import org.json.JSONObject;
 import org.mifos.connector.ams.pesacore.pesacore.dto.PesacoreRequestDTO;
 import org.mifos.connector.ams.pesacore.util.ConnectionUtils;
+import org.mifos.connector.ams.pesacore.util.PesacoreUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -55,6 +56,29 @@ public class PesaRouteBuilder extends RouteBuilder {
                 })
                 .to("direct:transfer-settlement-base");
 
+        from("rest:POST:/api/v1/paybill/validate/roster")
+                .id("validate-user")
+                .log(LoggingLevel.INFO, "## Roster user validation")
+                .setBody(e -> {
+                    String body=e.getIn().getBody(String.class);
+                    logger.debug("Body : {}",body);
+                    e.setProperty("dfspId",e.getProperty("dfspId"));
+                    return body;
+                })
+                .to("direct:transfer-validation-base")
+                .process(e->{
+                    String transactionId= e.getProperty(TRANSACTION_ID).toString();
+                    logger.debug("Transaction Id : "+transactionId);
+                    logger.debug("Response received from validation base : {}",e.getIn().getBody());
+                    // Building the response
+                    JSONObject responseObject=new JSONObject();
+                    responseObject.put("reconciled", e.getProperty(PARTY_LOOKUP_FAILED).equals(false));
+                    responseObject.put("AMS", "roster");
+                    responseObject.put("transaction_id", transactionId);
+                    logger.debug("response object "+responseObject);
+                    e.getIn().setBody(responseObject.toString());
+                });
+
         from("direct:transfer-validation-base")
                 .id("transfer-validation-base")
                 .log(LoggingLevel.INFO, "## Starting transfer Validation base route")
@@ -65,12 +89,16 @@ public class PesaRouteBuilder extends RouteBuilder {
                 .process(exchange -> {
                     // processing success case
                     exchange.setProperty(PARTY_LOOKUP_FAILED, false);
+                    exchange.setProperty("dfspId",exchange.getProperty("dfspId"));
+                    logger.debug("Pesacore Validation Success");
                 })
                 .otherwise()
                 .log(LoggingLevel.ERROR, "Validation unsuccessful")
                 .process(exchange -> {
                     // processing unsuccessful case
                     exchange.setProperty(PARTY_LOOKUP_FAILED, true);
+                    exchange.setProperty("dfspId",exchange.getProperty("dfspId"));
+                    logger.debug("Pesacore Validation Failure");
                 });
 
         from("direct:transfer-validation")
@@ -81,14 +109,26 @@ public class PesaRouteBuilder extends RouteBuilder {
                 .setHeader("Content-Type", constant("application/json"))
                 .setHeader("Authorization", simple("Token " + authHeader))
                 .setBody(exchange -> {
-                    JSONObject channelRequest = (JSONObject) exchange.getProperty(CHANNEL_REQUEST);
-                    String transactionId = exchange.getProperty(TRANSACTION_ID, String.class);
+                    if(exchange.getProperty(CHANNEL_REQUEST)!=null) {
+                        JSONObject channelRequest = (JSONObject) exchange.getProperty(CHANNEL_REQUEST);
+                        String transactionId = exchange.getProperty(TRANSACTION_ID, String.class);
 
-                    PesacoreRequestDTO verificationRequestDTO = buildPesacoreDtoFromChannelRequest(channelRequest,
-                            transactionId);
+                        PesacoreRequestDTO verificationRequestDTO = buildPesacoreDtoFromChannelRequest(channelRequest,
+                                transactionId);
+                        logger.debug("Validation request DTO: \n\n\n" + verificationRequestDTO);
+                        return verificationRequestDTO;
+                    }
+                    else {
+                        JSONObject paybillRequest = new JSONObject(exchange.getIn().getBody(String.class));
+                        PesacoreRequestDTO pesacoreRequestDTO = PesacoreUtils.convertPaybillPayloadToAmsPesacorePayload(paybillRequest);
 
-                    logger.info("Validation request DTO: \n\n\n" + verificationRequestDTO);
-                    return verificationRequestDTO;
+                        String transactionId = pesacoreRequestDTO.getRemoteTransactionId();
+                        log.info(pesacoreRequestDTO.toString());
+                        exchange.setProperty(TRANSACTION_ID, transactionId);
+                        exchange.setProperty("dfspId",exchange.getProperty("dfspId"));
+                        logger.debug("Validation request DTO: \n\n\n" + pesacoreRequestDTO);
+                        return pesacoreRequestDTO;
+                    }
                 })
                 .marshal().json(JsonLibrary.Jackson)
                 .toD(getVerificationEndpoint()+ "?bridgeEndpoint=true&throwExceptionOnFailure=false&"+
@@ -129,18 +169,28 @@ public class PesaRouteBuilder extends RouteBuilder {
                 .setHeader("Content-Type", constant("application/json"))
                 .setHeader("Authorization", simple("Token " + authHeader))
                 .setBody(exchange -> {
+                    if(exchange.getProperty(CHANNEL_REQUEST).toString().contains("customData")){
+                        JSONObject channelRequest = (JSONObject) exchange.getProperty(CHANNEL_REQUEST);
+                        String transactionId = exchange.getProperty(TRANSACTION_ID, String.class);
+                        String mpesaReceiptNumber = exchange.getProperty(EXTERNAL_ID, String.class);
 
-                    JSONObject channelRequest = (JSONObject) exchange.getProperty(CHANNEL_REQUEST);
-                    String transactionId = exchange.getProperty(TRANSACTION_ID, String.class);
-                    String mpesaReceiptNumber = exchange.getProperty(EXTERNAL_ID, String.class);
+                        PesacoreRequestDTO confirmationRequestDTO = buildPesacoreDtoFromChannelRequest(channelRequest,
+                                mpesaReceiptNumber);
+                        confirmationRequestDTO.setStatus("successful");
+                        confirmationRequestDTO.setReceiptId(mpesaReceiptNumber);
 
-                    PesacoreRequestDTO confirmationRequestDTO = buildPesacoreDtoFromChannelRequest(channelRequest,
-                            mpesaReceiptNumber);
-                    confirmationRequestDTO.setStatus("successful");
-                    confirmationRequestDTO.setReceiptId(mpesaReceiptNumber);
+                        logger.info("Confirmation request DTO: \n\n\n" + confirmationRequestDTO);
+                        return confirmationRequestDTO;
+                    }else {
+                        JSONObject paybillRequest = new JSONObject(exchange.getIn().getBody(String.class));
+                        PesacoreRequestDTO pesacoreRequestDTO = PesacoreUtils.convertPaybillPayloadToAmsPesacorePayload(paybillRequest);
 
-                    logger.info("Confirmation request DTO: \n\n\n" + confirmationRequestDTO);
-                    return confirmationRequestDTO;
+                        String transactionId = pesacoreRequestDTO.getRemoteTransactionId();
+                        log.debug(pesacoreRequestDTO.toString());
+                        exchange.setProperty(TRANSACTION_ID, transactionId);
+                        logger.debug("Confirmation request DTO: {}" ,pesacoreRequestDTO);
+                        return pesacoreRequestDTO;
+                    }
                 })
                 .marshal().json(JsonLibrary.Jackson)
                 .toD(getConfirmationEndpoint() + "?bridgeEndpoint=true&throwExceptionOnFailure=false&"+
@@ -148,6 +198,7 @@ public class PesaRouteBuilder extends RouteBuilder {
                 .log(LoggingLevel.INFO, "Pesacore verification api response: \n\n..\n\n..\n\n.. ${body}");
 
     }
+
 
     // returns the complete URL for verification request
     private String getVerificationEndpoint() {
